bd1601fd017f6e48bae14e9e591621483fe6dc8d
Katrin Schäfers <online@kschaefers.de>
2015-04-21 17:45:37 +0200

    added end version for live demo

---
+++ nodeLiveExample/finalVersion/index.js
@@ -0,0 +1,14 @@
+function go() {
+    var server = require("./server");
+    var router = require("./router");
+    var requestHandlers = require("./requestHandlers");
+
+    var handle = {};
+    handle["/"] = requestHandlers.start;
+    handle["/start"] = requestHandlers.start;
+    handle["/upload"] = requestHandlers.upload;
+
+    server.start(router.route, handle);
+}
+
+exports.go = go;
